3cc73ce2e84ac71c0ae9969b3ffc224eb5f1e03d
Miki Meerson <miki.meerson@mail.huji.ac.il>
2025-07-23 11:12:17 +0300

    add roi analysis

---
+++ roi_analyzer.py
@@ -0,0 +1,90 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import gridspec
+from matplotlib.widgets import LassoSelector
+from skimage.draw import polygon2mask
+
+
+def get_average_image(movie, stimulus_data_available=False):
+    """ Present a 2D image of mean value for each pixel across time """
+    plt.figure(figsize=(10, 10))
+    average_image = np.mean(movie, axis=0)
+
+    im = plt.imshow(average_image, cmap='gray')
+    plt.colorbar()
+    plt.title('Average image')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+
+    # Baseline correction - suppress low intensity background
+    baseline_image = np.percentile(average_image, 20)
+    average_image -= baseline_image
+
+    return average_image
+
+
+class ROISelector:
+    def __init__(self, image):
+        self.image = image
+        self.fig, self.ax = plt.subplots()
+        self.ax.imshow(image, cmap='gray')
+        self.canvas = self.ax.figure.canvas
+
+        self.roi_masks = []
+        self.roi_vertices = []
+
+        self.lasso = LassoSelector(self.ax, onselect=self.on_select)
+        plt.title("Draw ROI with mouse. Close window when done.")
+        plt.show()
+
+    def on_select(self, vertices):
+        # Save vertices
+        self.roi_vertices.append(vertices)
+
+        # Convert polygon to binary mask
+        mask = polygon2mask(self.image.shape, vertices)
+        self.roi_masks.append(mask)
+
+        # Show the selected ROI on the image
+        patch = plt.Polygon(vertices, closed=True, fill=False, edgecolor='red', linewidth=1.5)
+        self.ax.add_patch(patch)
+        self.canvas.draw_idle()
+
+    def get_roi_data(self):
+        return self.roi_masks, self.roi_vertices
+
+
+def extract_traces(movie, masks, avg_img):
+    n_frames = movie.shape[0]
+    n_rois = len(masks)
+    traces = np.zeros((n_frames, n_rois))
+    f0 = np.zeros(n_rois)
+
+    for i, mask in enumerate(masks):
+        # Average intensity within mask for each frame
+        roi_pixels = movie[:, mask]
+        traces[:, i] = roi_pixels.mean(axis=1)
+        f0[i] = avg_img[mask].mean() # Get baseline per ROI
+
+    # ΔF/F calculation
+    dff_traces = traces / f0[np.newaxis, :]
+
+    fig, ax = plt.subplots(figsize=(12, 6))
+    for i in range(n_rois):
+        ax.plot(dff_traces[:, i], label=f"ROI {i + 1}")
+    ax.set_title("ΔF/F traces")
+    ax.set_xlabel("Frame")
+    ax.set_ylabel("ΔF/F")
+    ax.legend()
+    plt.tight_layout()
+
+    return traces
+
+
+def roi_analysis(movie):
+    avg_img = get_average_image(movie)
+    selector = ROISelector(avg_img)
+    roi_masks, roi_vertices = selector.get_roi_data()
+
+    roi_traces = extract_traces(movie, roi_masks, avg_img)
+    return roi_masks, roi_traces
---
+++ temp.py
@@ -0,0 +1,111 @@
+
+# TODO temporarily store here all code related to blue light periods since it's irrelevant to current data
+
+from matplotlib import pyplot as plt
+
+from clean_pipeline import compute_intensity, regress_out_poly2
+from experiment_constants import *
+
+
+def get_average_image(movie, stimulus_data_available=False):
+    """ Present a 2D image of mean value for each pixel across time """
+    fig, ax = plt.subplots(1, 2, figsize=(20, 4))
+
+    average_image = np.mean(movie, axis=0)
+
+    im_original = ax[0].imshow(average_image, cmap='gray')
+    fig.colorbar(im_original, ax=ax[0])
+    ax[0].set_title('Average image')
+    ax[0].set_xlabel('X')
+    ax[0].set_ylabel('Y')
+
+    # Baseline correction - suppress low intensity background
+    baseline_image = np.percentile(average_image, 20)
+    average_image -= baseline_image
+
+    im_corrected = ax[1].imshow(average_image, cmap='gray')
+    fig.colorbar(im_corrected, ax=ax[1])
+    ax[1].set_title('Average image With Baseline Correction')
+    ax[1].set_xlabel('X')
+    ax[1].set_ylabel('Y')
+
+    plt.show()
+
+    if stimulus_data_available:
+        # Get average images during interesting periods
+        avg_img_stim_on = np.mean(movie[STIM_ON_PERIOD, :, :], axis=0)
+        avg_img_stim_off = np.mean(movie[STIM_OFF_PERIOD, :, :], axis=0)
+
+        # Baseline correction
+        avg_img_stim_on -= np.percentile(avg_img_stim_on, 20)
+        avg_img_stim_off -= np.percentile(avg_img_stim_off, 20)
+
+        fig, ax = plt.subplots(1, 2, figsize=(20, 4))
+
+        im_stim_on = ax[0].imshow(avg_img_stim_on, cmap='gray')
+        fig.colorbar(im_stim_on, ax=ax[0])
+        ax[0].set_title('Average Image During Stimulus ON')
+        ax[0].set_xlabel('X')
+        ax[0].set_ylabel('Y')
+
+        im_stim_off = ax[1].imshow(avg_img_stim_off, cmap='gray')
+        fig.colorbar(im_stim_off, ax=ax[1])
+        ax[1].set_title('Average Image During Stimulus OFF')
+        ax[1].set_xlabel('X')
+        ax[1].set_ylabel('Y')
+
+        plt.show()
+
+    return average_image
+
+
+def get_average_images_during_stimulus(movie):
+    assert len(BLUE_ON) == len(BLUE_OFF), "Amount of stimulus start and end time points should be the same"
+
+    blocks_num = len(BLUE_ON)
+    num_epochs = len(BLUE_OFF)
+    blue_on_periods = np.array([np.arange(start + FRAMES_TO_SKIP, start + LIGHT_STIMULATION_DURATION) for start in BLUE_ON])
+    blue_off_periods = np.array([np.arange(start + FRAMES_TO_SKIP, start + LIGHT_STIMULATION_DURATION) for start in BLUE_OFF])
+
+    intensity = compute_intensity(movie)
+    t = np.arange(len(intensity))
+
+    plt.figure(figsize=(10, 10))
+    plt.plot(t, intensity, 'k-')
+    for i in range(num_epochs):
+        plt.plot(blue_on_periods[i], intensity[blue_on_periods[i]], 'b-')
+        plt.plot(blue_off_periods[i], intensity[blue_off_periods[i]], 'r-')
+    plt.title("Intensity with Blue On/Off Periods")
+    plt.show()
+
+    # Build stimulus-aligned movie blocks
+    nframes, nrow, ncol = movie.shape
+    movie_on_blocks = np.zeros((blocks_num, BLOCK_LENGTH, nrow, ncol))
+    movie_off_blocks = np.zeros_like(movie_on_blocks)
+
+    for i in range(blocks_num):
+        movie_on = movie[blue_on_periods[i]]
+        movie_off = movie[blue_off_periods[i]]
+
+        # TODO understand why in original code skipped 50 and not 80
+        movie_on_blocks[i] = movie_on[FRAMES_TO_SKIP:BLOCK_LENGTH + FRAMES_TO_SKIP]
+        movie_off_blocks[i] = movie_off[FRAMES_TO_SKIP:BLOCK_LENGTH + FRAMES_TO_SKIP]
+
+        # TODO understand this
+        # align epochs across time (for consistency)
+        if i > 0:
+            movie_on_blocks[i] -= np.mean(movie_on_blocks[i, :100], axis=0)
+            movie_off_blocks[i] -= np.mean(movie_off_blocks[i, :100], axis=0)
+
+    # TODO understand this
+    movie_on_blocks = movie_on_blocks.transpose(0, 2, 3, 1).reshape((-1, nrow, ncol))
+    movie_off_blocks = movie_off_blocks.transpose(0, 2, 3, 1).reshape((-1, nrow, ncol))
+
+    movie_on_blocks = regress_out_poly2(movie_on_blocks)
+    movie_off_blocks = regress_out_poly2(movie_off_blocks)
+
+    plt.figure()
+    plt.plot(np.mean(movie_on_blocks, axis=(1, 2)), label="Blue On")
+    plt.plot(np.mean(movie_off_blocks, axis=(1, 2)), label="Blue Off")
+    plt.legend()
+    plt.title("Intensity After Drift Removal")
